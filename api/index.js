a6ac57b7986fb049213d2d4514d2ed2c8ea58a69
samme <olalekanadeniyi081@gmail.com>
2025-12-13 01:05:24 +0100

    vercelupdate

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 // Serverless function wrapper for Vercel
 const app = require('../sec/index');
 
-module.exports = app;
+// Export as Vercel serverless function
+module.exports = (req, res) => {
+  return app(req, res);
+};
 
